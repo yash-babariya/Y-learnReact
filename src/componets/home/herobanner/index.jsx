1bf0b98d7d25fd5f432a02a0f3b01c77968bcc94
yash-babariya <yashbabariya549@gmail.com>
2024-05-24 17:48:04 +0530

    add hero-bamnner

--- src/componets/home/herobanner/index.jsx
+++ src/componets/home/herobanner/index.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import './herobaner.scss'
+import logo from '../../../assets/logo/logo.png'
 
 export default function HeroBanner() {
     return (
         <div className='hero-banner'>
             <div className="container">
                 <section>
+                    <div className="logo">
+                        <img src={logo} alt="logo" />
+                    </div>
+                    <div className="hero-text">
 
+                        <div className="hero-buttons">
+                            <button className='solid'>Learn More</button>
+                            <button className='border'>Get Started</button>
+                        </div>
+                    </div>
                 </section>
             </div>
         </div>
