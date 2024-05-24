d084912c92c2a86c9673bbf5f683ec7e72f0e052
yash-babariya <yashbabariya549@gmail.com>
2024-05-24 15:19:48 +0530

    c

--- src/componets/home/index.jsx
+++ src/componets/home/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import HeroBanner from './herobanner'
 
 export default function Home() {
     return (
         <div>
-            hellom this is home
+            <HeroBanner />
         </div>
     )
 }
