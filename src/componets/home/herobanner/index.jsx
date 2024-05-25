c398b76af5ff74f41fb14f4a004f29d38d1849f3
yash-babariya <yashbabariya549@gmail.com>
2024-05-25 11:22:47 +0530

    some changeas

--- src/componets/home/herobanner/index.jsx
+++ src/componets/home/herobanner/index.jsx
@@ -7,14 +7,17 @@ export default function HeroBanner() {
         <div className='hero-banner'>
             <div className="container">
                 <section>
-                    <div className="logo">
-                        <img src={logo} alt="logo" />
-                    </div>
                     <div className="hero-text">
-                        <div className="hero-buttons">
-                            <button className='solid'>Learn More</button>
-                            <button className='border'>Get Started</button>
+                        <div className="logo">
+                            <img src={logo} alt="logo" />
                         </div>
+                        <div className="haeding">
+                            <h1>Build <span>Reusable</span> Frontend Components in 3 Minutes</h1>
+                        </div>
+                    </div>
+                    <div className="hero-buttons">
+                        <button className='solid'>Learn More</button>
+                        <button className='border'>Get Started</button>
                     </div>
                 </section>
             </div>
